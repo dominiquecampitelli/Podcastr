1ea3252480fd1ba1028d43b6e0c1f6f94a1e477b
Lais Campitelli <laiscamp@outlook.com>
2021-04-25 15:59:30 -0300

    Slider and display

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -2,11 +2,11 @@ import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { GetStaticPaths, GetStaticProps } from "next";
 
 import { api } from "../../services/api";
 import { convertDurationToTimeString } from "../../utils/convertDurationToTimeString";
+import { usePlayer } from "../../contexts/PlayerContext";
 
 import styles from "./episode.module.scss";
 
@@ -27,11 +27,7 @@ type EpisodeProps = {
 };
 
 export default function Episode({ episode }: EpisodeProps) {
-  const router = useRouter();
-
-  if (router.isFallback) {
-    return <p>Carregando...</p>;
-  }
+  const { play } = usePlayer();
 
   return (
     <div className={styles.episode}>
@@ -47,7 +43,7 @@ export default function Episode({ episode }: EpisodeProps) {
           src={episode.thumbnail}
           objectFit="cover"
         />
-        <button type="button">
+        <button type="button" onClick={() => play(episode)}>
           <img src="/play.svg" alt="Tocar episódio" />
         </button>
       </div>
